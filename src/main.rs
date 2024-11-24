35eeccfda909400ef822720a32460ae3e0046a47
SAMoosavi <moosavi238@gmail.com>
2024-11-24 10:51:40 +0330

    use parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod parser;
+
+use crate::parser::Expiration;
+
 fn main() {
-    println!("Hello, world!");
+    let ex = Expiration::new("(([3+x_2]+2)+(2-x))-5");
+    match ex.pars() {
+        Ok(_) => {}
+        Err(x) => {
+            println!("{x}")
+        }
+    };
 }
