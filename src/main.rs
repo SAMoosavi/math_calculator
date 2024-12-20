1ba3a4fd2bfc0f9d9860357d84f9ad1574d838d3
SAMoosavi <moosavi238@gmail.com>
2024-12-20 13:07:12 +0330

    use chumsky

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 mod parser;
 
-use crate::parser::Expiration;
+use parser::Expiration;
 
 fn main() {
-    let mut a = String::from(" ( 3 + x_2 + 2 ) + ( 2 - x ) - ( [ 3 + x_2 ] + 2 )");
-    for _ in 1..22 {
-        a = format!("( {a} ) + ( {a} )")
-    }
-    let ex = Expiration::new(&a);
-    match ex {
-        Ok(x) => {
-            println!("{}", x);
-        }
-        Err(x) => {
-            println!("{x}")
-        }
+    let mut a = String::from("- 3 + x * 2 + y ^ (x * 3) + y / 2");
+
+    (1..20)
+        .into_iter()
+        .for_each(|_| a = format!("({a}) + ({a})"));
+
+    let src = format!("let x = 10; let y = 20; {a}");
+    match Expiration::new(&src) {
+        Ok(exp) => match exp.calculate() {
+            Ok(output) => {
+                println!("answer: {}", output)
+            }
+            Err(eval_err) => println!("Evaluation error: {}", eval_err),
+        },
+        Err(e) => println!("Parse error: {}", e),
     };
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,111 +1,164 @@
-mod element;
-mod scope_marker;
+use std::sync::{Arc, Mutex};
 
-use element::Element;
-use scope_marker::ScopeMarker;
+use chumsky::prelude::*;
 
-use std::fmt::Display;
+#[derive(Debug)]
+enum Expr<'a> {
+    Num(f64),
+    Var(&'a str),
 
-pub struct Expiration {
-    ex: Element,
+    Neg(Box<Expr<'a>>),
+    Add(Box<Expr<'a>>, Box<Expr<'a>>),
+    Sub(Box<Expr<'a>>, Box<Expr<'a>>),
+    Mul(Box<Expr<'a>>, Box<Expr<'a>>),
+    Div(Box<Expr<'a>>, Box<Expr<'a>>),
+    Pow(Box<Expr<'a>>, Box<Expr<'a>>),
+
+    Let {
+        name: &'a str,
+        rhs: Box<Expr<'a>>,
+        then: Box<Expr<'a>>,
+    },
 }
 
-impl Display for Expiration {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "'{}'", self.ex)
-    }
+pub struct Expiration<'a> {
+    ast: Expr<'a>,
 }
 
-impl Expiration {
-    pub fn new(ex_str: &str) -> Result<Self, String> {
-        Ok(Self {
-            ex: Self::pars(ex_str)?,
-        })
-    }
-
-    fn create_operator(stack: &mut Vec<Element>, operator_stack: &mut Vec<String>) {
-        let right = stack.pop().unwrap();
-        let operator = &operator_stack.pop().unwrap();
-        let left = stack.pop().unwrap();
-        stack.push(Element::from_operator(left, operator, right));
-    }
-
-    fn pars(ex_str: &str) -> Result<Element, String> {
-        let mut stack = Vec::new();
-        let mut operator_stack: Vec<String> = Vec::new();
-
-        for x in ex_str.split_whitespace() {
-            match x {
-                "-" | "+" => {
-                    let check_precedence = |operator_stack: &Vec<String>| {
-                        if let Some(x) = operator_stack.last() {
-                            matches!(x.as_str(), "*" | "-" | "^")
-                        } else {
-                            false
-                        }
-                    };
-
-                    while check_precedence(&operator_stack) {
-                        Self::create_operator(&mut stack, &mut operator_stack);
-                    }
-                    operator_stack.push(x.to_string());
-                }
-                "*" | "/" => {
-                    let check_precedence = |operator_stack: &Vec<String>| {
-                        if let Some(x) = operator_stack.last() {
-                            matches!(x.as_str(), "^")
-                        } else {
-                            false
-                        }
-                    };
-                    while check_precedence(&operator_stack) {
-                        Self::create_operator(&mut stack, &mut operator_stack);
-                    }
-                    operator_stack.push(x.to_string());
-                }
-                "^" => {
-                    operator_stack.push(x.to_string());
-                }
-                x if ScopeMarker::is_close(&ScopeMarker::from_str(x)) => {
-                    let scope = ScopeMarker::from_str(x);
-
-                    let check_open =
-                        |stack: &mut Vec<Element>, operator_stack: &mut Vec<String>| {
-                            if let Some(x) = operator_stack.last() {
-                                let current_scope = ScopeMarker::from_str(x);
-                                if scope.same_scope(&current_scope) {
-                                    operator_stack.pop().unwrap();
-                                    let lase_element = stack.pop().unwrap();
-                                    let len = stack.len();
-                                    stack[len - 1] = lase_element;
-                                    false
-                                } else {
-                                    true
-                                }
-                            } else {
-                                false
-                            }
-                        };
-
-                    while check_open(&mut stack, &mut operator_stack) {
-                        Self::create_operator(&mut stack, &mut operator_stack);
-                    }
-                }
-                x if ScopeMarker::is_open(&ScopeMarker::from_str(x)) => {
-                    stack.push(Element::from_var(x.to_string()));
-                    operator_stack.push(x.to_string());
-                }
-                _ => match x.parse() {
-                    Ok(num) => stack.push(Element::from_val(num)),
-                    Err(_) => stack.push(Element::from_var(x.to_string())),
-                },
+impl<'a> Expiration<'a> {
+    pub fn new(input: &'a str) -> Result<Self, String> {
+        match Self::parse().parse(&input).into_result() {
+            Ok(ast) => Ok(Self { ast }),
+            Err(parse_errs) => {
+                let mut er = String::new();
+                parse_errs
+                    .into_iter()
+                    .for_each(|e| er = format!("{er}\nParse error: {e}"));
+                Err(er)
             }
         }
+    }
 
-        while stack.len() != 1 {
-            Self::create_operator(&mut stack, &mut operator_stack);
+    fn parse() -> impl Parser<'a, &'a str, Expr<'a>> {
+        let ident = text::ascii::ident().padded();
+
+        let expr = recursive(|expr| {
+            let int = text::int(10).map(|s: &str| Expr::Num(s.parse().unwrap()));
+
+            let atom = int
+                .or(expr.delimited_by(just('('), just(')')))
+                .or(ident.map(Expr::Var))
+                .padded();
+
+            let op = |c| just(c).padded();
+
+            let unary = op('-')
+                .repeated()
+                .foldr(atom, |_op, rhs| Expr::Neg(Box::new(rhs)));
+
+            let power = unary.clone().foldl(
+                choice((op('^').to(Expr::Mul as fn(_, _) -> _),))
+                    .then(unary)
+                    .repeated(),
+                |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
+            );
+
+            let product = power.clone().foldl(
+                choice((
+                    op('*').to(Expr::Mul as fn(_, _) -> _),
+                    op('/').to(Expr::Div as fn(_, _) -> _),
+                ))
+                .then(power)
+                .repeated(),
+                |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
+            );
+
+            let sum = product.clone().foldl(
+                choice((
+                    op('+').to(Expr::Add as fn(_, _) -> _),
+                    op('-').to(Expr::Sub as fn(_, _) -> _),
+                ))
+                .then(product)
+                .repeated(),
+                |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
+            );
+
+            sum
+        });
+
+        let decl = recursive(|decl| {
+            let r#let = text::ascii::keyword("let")
+                .ignore_then(ident)
+                .then_ignore(just('='))
+                .then(expr.clone())
+                .then_ignore(just(';'))
+                .then(decl.clone())
+                .map(|((name, rhs), then)| Expr::Let {
+                    name,
+                    rhs: Box::new(rhs),
+                    then: Box::new(then),
+                });
+
+            r#let.or(expr).padded()
+        });
+
+        decl
+    }
+
+    pub fn calculate(&'a self) -> Result<f64, String> {
+        Self::eval(&self.ast, Arc::new(Mutex::new(Vec::new())))
+    }
+
+    fn binary_operator(
+        l: &'a Expr<'a>,
+        r: &'a Expr<'a>,
+        vars: Arc<Mutex<Vec<(&'a str, f64)>>>,
+        f: fn(f64, f64) -> f64,
+    ) -> Result<f64, String> {
+        let (left_result, right_result) = rayon::join(
+            || Self::eval(l, vars.clone()),
+            || Self::eval(r, vars.clone()),
+        );
+
+        let left = left_result?;
+        let right = right_result?;
+
+        Ok(f(left, right))
+    }
+
+    fn eval(expr: &'a Expr<'a>, vars: Arc<Mutex<Vec<(&'a str, f64)>>>) -> Result<f64, String> {
+        match expr {
+            Expr::Num(x) => Ok(*x),
+            Expr::Neg(a) => Ok(-Self::eval(a, vars)?),
+            Expr::Add(a, b) => Self::binary_operator(&a, &b, vars, |x, y| x + y),
+            Expr::Sub(a, b) => Self::binary_operator(&a, &b, vars, |x, y| x - y),
+            Expr::Mul(a, b) => Self::binary_operator(&a, &b, vars, |x, y| x * y),
+            Expr::Div(a, b) => Self::binary_operator(&a, &b, vars, |x, y| x / y),
+            Expr::Pow(a, b) => Self::binary_operator(&a, &b, vars, |x, y| x.powf(y)),
+            Expr::Var(name) => {
+                let vars = vars.lock().unwrap();
+                if let Some((_, val)) = vars.iter().rev().find(|(var, _)| var == name) {
+                    Ok(*val)
+                } else {
+                    Err(format!("Cannot find variable `{}` in scope", name))
+                }
+            }
+            Expr::Let { name, rhs, then } => {
+                let rhs_value = Self::eval(rhs, vars.clone())?;
+
+                {
+                    let mut vars = vars.lock().unwrap();
+                    vars.push((*name, rhs_value));
+                }
+                let output = Self::eval(then, vars.clone());
+
+                {
+                    let mut vars = vars.lock().unwrap();
+                    vars.pop();
+                }
+
+                output
+            }
         }
-
-        Ok(stack.pop().unwrap())
     }
 }
